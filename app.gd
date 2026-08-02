3df0115053075ffa9f5f6812da19940890c9a520
kingptvoi <73782299+kingptvoi@users.noreply.github.com>
2026-08-02 13:05:53 +0300

    added instructions for saving and loading files

--- app.gd
+++ app.gd
@@ -58,6 +58,9 @@ func _input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed and dragging:
 			DisplayServer.window_start_drag()
+			
+#func initialise_display():
+	#DisplayServer.window_set_size(Vector2i(DisplayServer.screen_get_size() / 4))
 
 #func _on_title_background_mouse_entered() -> void:
 	#dragging = true
